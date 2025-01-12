8efa30bcef83eb17ea59ae428310a506f592b22c
khurram-iqbal-tech <khurram.ms1@gmail.com>
2025-01-12 12:12:42 +0500

    Initial Commit

---
+++ gitTest/gitTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  gitTestApp.swift
+//  gitTest
+//
+//  Created by MacBook Pro on 12/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct gitTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ gitTestTests/gitTestTests.swift
@@ -0,0 +1,17 @@
+//
+//  gitTestTests.swift
+//  gitTestTests
+//
+//  Created by MacBook Pro on 12/01/2025.
+//
+
+import Testing
+@testable import gitTest
+
+struct gitTestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
